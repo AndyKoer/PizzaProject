c1d718df662bb96028d551a688a6a25e20bdd245
Daniel Crook <135056261+Daniel-EDC@users.noreply.github.com>
2024-04-29 20:31:41 -0500

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -4,13 +4,27 @@
 session_start();
 ?>
 
+<style>
+
+	.myText
+	{
+		font-family:Georgia;
+		font-size:50px;
+		text-decoration:none;
+		font-style: italic;
+		font-weight:bold;
+		color:blue;
+	}
+
+</style>
+
 <html>
 <head>
     <title>Order Confirmation</title>
 </head>
-<body>
+<body bgcolor="EECB1F" >
     <div style="text-align: center;">
-        <p>Thank you, <?php echo $_SESSION['username']; ?>, your order is confirmed!</p>
+        <p class="myText">Thank you, <?php echo $_SESSION['username']; ?>, your order is confirmed!</p>
     </div>
 </body>
 </html>
